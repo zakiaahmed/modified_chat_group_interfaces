18e3717b1a79b00b89d86cf381061f19310a4ebc
zakiaahmed <57689658+zakiaahmed@users.noreply.github.com>
2019-12-14 15:00:19 +0200

    Add files via upload

---
+++ Chat_group.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat_group;
+
+/**
+ *
+ * @author PIXEL-PC
+ */
+public class Chat_group {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Login_forme login_forme= new Login_forme();
+    login_forme.setVisible(true);
+    }
+    
+}
